ef70f4f637e0efbc911e6fa6d4fa31f5b706bac4
kemaldemirgil <kemal.demirgil@hotmail.com>
2021-08-12 13:33:23 -0400

    adding loader

--- client/src/pages/CityResult Page/index.js
+++ client/src/pages/CityResult Page/index.js
@@ -22,6 +22,12 @@ import { QUERY_ME } from "../../utils/queries";
 import { ToastsContainer, ToastsStore } from "react-toasts";
 
 const CityResult = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 6000);
+  }, []);
+
   let singleCity;
   let cityName = "";
   let cityID = window.location.pathname.split("/").pop();
@@ -134,218 +140,243 @@ const CityResult = () => {
   return (
     <div>
       <Nav />
-      <ToastsContainer store={ToastsStore} />
-      <div className="citypage">
-        <div className="citypage-intro">
-          <Grid stackable divided="vertically">
-            <Grid.Row columns={2}>
-              <Grid.Column>
-                <img
+      {loading === false ? (
+        <div>
+          <ToastsContainer store={ToastsStore} />
+          <div className="citypage">
+            <div className="citypage-intro">
+              <Grid stackable divided="vertically">
+                <Grid.Row columns={2}>
+                  <Grid.Column>
+                    <img
+                      data-aos="zoom-out-down"
+                      src={singleCity.image}
+                      alt="City"
+                      className="citypage-cityimage"
+                    />
+                    <Button
+                      data-aos="zoom-out-left"
+                      style={{ marginTop: "2rem" }}
+                      as="div"
+                      labelPosition="right"
+                      onClick={addFavorite}
+                    >
+                      <Button color="red">
+                        <Icon name="favorite" />
+                        Add to favorites
+                      </Button>
+                      <Label as="a" basic color="red" pointing="left">
+                        {user.favorites ? user.favorites.length : ""}
+                      </Label>
+                    </Button>
+                    <Button
+                      data-aos="zoom-out-right"
+                      onClick={randomCity}
+                      color="teal"
+                      animated="fade"
+                      style={{ margin: "1rem" }}
+                    >
+                      <Button.Content visible>
+                        Take me somewhere random
+                      </Button.Content>
+                      <Button.Content hidden>
+                        Discover
+                        <Icon name="paper plane" />
+                      </Button.Content>
+                    </Button>
+                  </Grid.Column>
+                  <Grid.Column textAlign="center">
+                    <h1 data-aos="zoom-in">{singleCity.city}</h1>
+                    <h3 data-aos="zoom-out">
+                      <Icon name="map marker alternate" />
+                      {singleCity.country}
+                    </h3>
+                    {cityWeatherData === undefined ||
+                    cityWeatherData.main === undefined ? (
+                      ""
+                    ) : (
+                      <div data-aos="zoom-out-up" className="city-weather">
+                        <Card color="teal">
+                          <Card.Content>
+                            <Card.Header className="weather-title">
+                              Current Temp:{" "}
+                              {parseInt(cityWeatherData.main.temp)}
+                              <sup>&#8451;</sup>
+                              <img
+                                src={`http://openweathermap.org/img/wn/${cityWeatherData.weather[0].icon}.png`}
+                                width="50"
+                                height="50"
+                                alt="logo"
+                              />
+                            </Card.Header>
+                            <Card.Meta>
+                              Feels Like:{" "}
+                              {parseInt(cityWeatherData.main.feels_like)}
+                              <sup>&#8451;</sup>
+                            </Card.Meta>
+                            <Card.Description>
+                              <div style={{ float: "left" }}>
+                                Max Temp:{" "}
+                                {parseInt(cityWeatherData.main.temp_max)}
+                                <sup>&#8451;</sup>
+                                <p>
+                                  Humidity: {cityWeatherData.main.humidity}%
+                                </p>
+                              </div>
+                              <div>
+                                Min Temp:{" "}
+                                {parseInt(cityWeatherData.main.temp_min)}
+                                <sup>&#8451;</sup>
+                                <p>
+                                  Wind Speed: {cityWeatherData.wind.speed}m/s
+                                </p>
+                              </div>
+                            </Card.Description>
+                          </Card.Content>
+                        </Card>
+                      </div>
+                    )}
+                    {cityCovidData !== undefined && (
+                      <Card data-aos="zoom-out-down" color="teal" centered>
+                        <Card.Content>
+                          <Card.Header>Covid Cases</Card.Header>
+                          <Card.Meta>{singleCity.country}</Card.Meta>
+                          <Card.Description textAlign="left">
+                            <b>
+                              <Icon name="heartbeat" />
+                              Deaths today:
+                              {cityCovidData.change.deaths} <br />
+                              <br />
+                              <Icon name="meh" />
+                              Active cases:
+                              {cityCovidData.change.active_cases}
+                            </b>
+                          </Card.Description>
+                        </Card.Content>
+                      </Card>
+                    )}
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </div>
+            <Container>
+              <Grid
+                stackable
+                divided="vertically"
+                className="city-video-container"
+              >
+                <Grid.Row columns={2}>
+                  <Grid.Column>
+                    <div data-aos="zoom-out-up">
+                      <Map />
+                    </div>
+                  </Grid.Column>
+                  <Grid.Column>
+                    <div data-aos="zoom-out-down" className="video-responsive">
+                      <iframe
+                        width="853"
+                        height="480"
+                        src={`https://www.youtube.com/embed/${singleCity.youtube}`}
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                        title="Embedded youtube"
+                      />
+                    </div>
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </Container>
+            <div className="title-hotel">
+              <h1 data-aos="zoom-out-up">Hotels &amp; Popular Attractions</h1>
+              {businesses !== undefined && (
+                <a
                   data-aos="zoom-out-down"
-                  src={singleCity.image}
-                  alt="City"
-                  className="citypage-cityimage"
-                />
-                <Button
-                  data-aos="zoom-out-left"
-                  style={{ marginTop: "2rem" }}
-                  as="div"
-                  labelPosition="right"
-                  onClick={addFavorite}
+                  target="_blank"
+                  rel="noreferrer"
+                  href={`https://www.yelp.com/search?find_desc=hotels&find_loc=${singleCity.city}`}
                 >
-                  <Button color="red">
-                    <Icon name="favorite" />
-                    Add to favorites
-                  </Button>
-                  <Label as="a" basic color="red" pointing="left">
-                    {user.favorites ? user.favorites.length : ""}
-                  </Label>
-                </Button>
-                <Button
-                  data-aos="zoom-out-right"
-                  onClick={randomCity}
-                  color="teal"
-                  animated="fade"
-                  style={{ margin: "1rem" }}
-                >
-                  <Button.Content visible>
-                    Take me somewhere random
-                  </Button.Content>
-                  <Button.Content hidden>
-                    Discover
-                    <Icon name="paper plane" />
-                  </Button.Content>
-                </Button>
-              </Grid.Column>
-              <Grid.Column textAlign="center">
-                <h1 data-aos="zoom-in">{singleCity.city}</h1>
-                <h3 data-aos="zoom-out">
-                  <Icon name="map marker alternate" />
-                  {singleCity.country}
-                </h3>
-                {cityWeatherData === undefined ||
-                cityWeatherData.main === undefined ? (
-                  ""
-                ) : (
-                  <div data-aos="zoom-out-up" className="city-weather">
-                    <Card color="teal">
-                      <Card.Content>
-                        <Card.Header className="weather-title">
-                          Current Temp: {parseInt(cityWeatherData.main.temp)}
-                          <sup>&#8451;</sup>
-                          <img
-                            src={`http://openweathermap.org/img/wn/${cityWeatherData.weather[0].icon}.png`}
-                            width="50"
-                            height="50"
-                            alt="logo"
-                          />
-                        </Card.Header>
-                        <Card.Meta>
-                          Feels Like:{" "}
-                          {parseInt(cityWeatherData.main.feels_like)}
-                          <sup>&#8451;</sup>
-                        </Card.Meta>
-                        <Card.Description>
-                          <div style={{ float: "left" }}>
-                            Max Temp: {parseInt(cityWeatherData.main.temp_max)}
-                            <sup>&#8451;</sup>
-                            <p>Humidity: {cityWeatherData.main.humidity}%</p>
-                          </div>
-                          <div>
-                            Min Temp: {parseInt(cityWeatherData.main.temp_min)}
-                            <sup>&#8451;</sup>
-                            <p>Wind Speed: {cityWeatherData.wind.speed}m/s</p>
-                          </div>
-                        </Card.Description>
-                      </Card.Content>
-                    </Card>
+                  <h4>Click here for more</h4>
+                </a>
+              )}
+            </div>
+            <div className="city-hotel-cards">
+              <Card.Group centered>
+                {businesses === undefined && (
+                  <div>
+                    <h3 data-aos="zoom-out-up">
+                      Couldn't get any information about this city
+                    </h3>
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <img
+                        data-aos="zoom-out-down"
+                        alt="city not found..."
+                        width="100%"
+                        height="auto"
+                        src="https://media.giphy.com/media/hPSMHzgHQeFuE/giphy.gif"
+                      />
+                    </div>
                   </div>
                 )}
-                {cityCovidData !== undefined && (
-                  <Card data-aos="zoom-out-down" color="teal" centered>
-                    <Card.Content>
-                      <Card.Header>Covid Cases</Card.Header>
-                      <Card.Meta>{singleCity.country}</Card.Meta>
-                      <Card.Description textAlign="left">
-                        <b>
-                          <Icon name="heartbeat" />
-                          Deaths today:
-                          {cityCovidData.change.deaths} <br />
-                          <br />
-                          <Icon name="meh" />
-                          Active cases:
-                          {cityCovidData.change.active_cases}
-                        </b>
-                      </Card.Description>
-                    </Card.Content>
-                  </Card>
-                )}
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
+                {businesses !== undefined &&
+                  businesses
+                    .filter((item, index) => index < 20)
+                    .map((business) => (
+                      <Card
+                        data-aos="zoom-out-left"
+                        color="blue"
+                        key={business.name}
+                        href={business.url}
+                      >
+                        {business.image_url === "" ? (
+                          <Image
+                            className="hotel-img"
+                            src="https://img2.10bestmedia.com/Images/Photos/378649/Park-Hyatt-New-York-Manhattan-Sky-Suite-Master-Bedroom-low-res_54_990x660.jpg"
+                          />
+                        ) : (
+                          <Image
+                            className="hotel-img"
+                            src={business.image_url}
+                          />
+                        )}
+                        <Card.Content>
+                          <Card.Header>{business.name}</Card.Header>
+                          <Card.Meta>Tel: {business.display_phone}</Card.Meta>
+                          <Card.Description>
+                            <p>Address:</p>
+                            <small>{business.location.address1}</small>
+                            <small>{business.location.address2}</small>
+                            <small>{business.location.address3}</small>
+                          </Card.Description>
+                        </Card.Content>
+                        <Card.Content extra>
+                          <Rating
+                            maxRating={5}
+                            defaultRating={business.rating}
+                            icon="star"
+                            disabled
+                          />
+                        </Card.Content>
+                      </Card>
+                    ))}
+              </Card.Group>
+            </div>
+          </div>
         </div>
-        <Container>
-          <Grid stackable divided="vertically" className="city-video-container">
-            <Grid.Row columns={2}>
-              <Grid.Column>
-                <div data-aos="zoom-out-up">
-                  <Map />
-                </div>
-              </Grid.Column>
-              <Grid.Column>
-                <div data-aos="zoom-out-down" className="video-responsive">
-                  <iframe
-                    width="853"
-                    height="480"
-                    src={`https://www.youtube.com/embed/${singleCity.youtube}`}
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                    title="Embedded youtube"
-                  />
-                </div>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Container>
-        <div className="title-hotel">
-          <h1 data-aos="zoom-out-up">Hotels &amp; Popular Attractions</h1>
-          {businesses !== undefined && (
-            <a
-              data-aos="zoom-out-down"
-              target="_blank"
-              rel="noreferrer"
-              href={`https://www.yelp.com/search?find_desc=hotels&find_loc=${singleCity.city}`}
-            >
-              <h4>Click here for more</h4>
-            </a>
-          )}
-        </div>
-        <div className="city-hotel-cards">
-          <Card.Group centered>
-            {businesses === undefined && (
-              <div>
-                <h3 data-aos="zoom-out-up">
-                  Couldn't get any information about this city
-                </h3>
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                >
-                  <img
-                    data-aos="zoom-out-down"
-                    alt="city not found..."
-                    width="100%"
-                    height="auto"
-                    src="https://media.giphy.com/media/hPSMHzgHQeFuE/giphy.gif"
-                  />
-                </div>
-              </div>
-            )}
-            {businesses !== undefined &&
-              businesses
-                .filter((item, index) => index < 20)
-                .map((business) => (
-                  <Card
-                    data-aos="zoom-out-left"
-                    color="blue"
-                    key={business.name}
-                    href={business.url}
-                  >
-                    {business.image_url === "" ? (
-                      <Image
-                        className="hotel-img"
-                        src="https://img2.10bestmedia.com/Images/Photos/378649/Park-Hyatt-New-York-Manhattan-Sky-Suite-Master-Bedroom-low-res_54_990x660.jpg"
-                      />
-                    ) : (
-                      <Image className="hotel-img" src={business.image_url} />
-                    )}
-                    <Card.Content>
-                      <Card.Header>{business.name}</Card.Header>
-                      <Card.Meta>Tel: {business.display_phone}</Card.Meta>
-                      <Card.Description>
-                        <p>Address:</p>
-                        <small>{business.location.address1}</small>
-                        <small>{business.location.address2}</small>
-                        <small>{business.location.address3}</small>
-                      </Card.Description>
-                    </Card.Content>
-                    <Card.Content extra>
-                      <Rating
-                        maxRating={5}
-                        defaultRating={business.rating}
-                        icon="star"
-                        disabled
-                      />
-                    </Card.Content>
-                  </Card>
-                ))}
-          </Card.Group>
-        </div>
-      </div>
+      ) : (
+        <>
+          <div className="loader-container">
+            <h3 className="loader-text"> </h3>
+          </div>
+        </>
+      )}
+
       <Footer />
     </div>
   );
